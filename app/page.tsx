871f054a289a5a41e600a8d7ab60f96fdf985e92
lqyha520 <30472964+lqyha520@users.noreply.github.com>
2025-03-12 12:07:00 +0800

    添加ContactSection组件

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Hero from '../components/Hero'
 import ProductSection from '../components/ProductSection'
 import SolutionSection from '../components/SolutionSection'
 import AboutSection from '../components/AboutSection'
-import ContactSection from '../components/ContactSection'
+import ContactSection from '../components/ContactSection.jsx'
 
 export default function Home() {
   return (
